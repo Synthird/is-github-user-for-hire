47a13d52a11a5ae74fb564ec03a6c845360db0ab
Synthird <105394020+Synthird@users.noreply.github.com>
2025-08-17 18:07:46 -0700

    Removed an empty line between express variables and their only function used

--- routes/is-for-hire.js
+++ routes/is-for-hire.js
@@ -2,7 +2,6 @@ import express from "express";
 import { isForHire } from "../controllers/for-hire-controller.js";
 
 const router = express.Router();
-
 router.get("/:username", isForHire);
 
 export default router;
--- server.js
+++ server.js
@@ -2,5 +2,4 @@ import express from "express";
 import forHireRouter from "./routes/is-for-hire.js";
 
 export const app = express();
-
 app.use("/api/is-for-hire", forHireRouter);
